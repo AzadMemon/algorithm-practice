8d30350abe9f3b949796bde01f5f0cea4a4e0dc1
Azad Memon <azad.memon@gmail.com>
2017-05-19 11:24:37 -0400

    Changed lib folder to be an actual java folder

--- lib/Stack.java
+++ lib/Stack.java
@@ -1,5 +1,5 @@
 public class Stack<T> {
-    private class StackElement<T> {
+    class StackElement<T> {
         public T data;
         public StackElement<T> next;
 
@@ -8,12 +8,12 @@ public class Stack<T> {
         }
     }
 
-    private T top;
+    public StackElement<T> top;
 
-    public StackElement<T> pop() {
+    public T pop() {
         StackElement<T> oldTop = top;
         top = top.next;
-        return oldTop;
+        return oldTop.data;
     }
 
     public void push(T data) {
